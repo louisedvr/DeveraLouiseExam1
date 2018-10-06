1adef119450d091c438e57e7581aaed34c7daf94
louisedvr <43290835+louisedvr@users.noreply.github.com>
2018-10-06 19:33:47 +0800

    Initial commit

---
+++ app/src/main/java/com/devera/louise/MainActivity.java
@@ -0,0 +1,13 @@
+package com.devera.louise;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
